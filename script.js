8873c9dba5fc6af653e362606bd10a0c81096071
PDGamerSG <pallab.das2024@vitstudent.ac.in>
2024-09-21 15:26:00 +0530

    deploy

--- script.js
+++ script.js
@@ -1,9 +1,2 @@
-var tabEl = document.querySelectorAll('a[data-bs-toggle="tab"]')
-tabEl.forEach(function (tab) {
-  tab.addEventListener('click', function () {
-    tabEl.forEach(function (el) {
-      el.classList.remove('active');
-    });
-    tab.classList.add('active');
-  });
-});  
+[InternetShortcut]
+URL=file:///c%3A/Users/mafif/OneDrive/Desktop/Programming/Java/Filter/script.js
