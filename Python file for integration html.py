815b86d564a79f7d38a4a3d844c4251882a09d34
PDGamerSG <pallab.das2024@vitstudent.ac.in>
2024-09-21 15:55:55 +0530

    Add files via upload

---
+++ Python file for integration html.py
@@ -0,0 +1,18 @@
+
+from flask import Flask,render_template
+
+
+
+app = Flask(__name__,template_folder="templates")
+
+
+@app.route('/')
+def first():
+    return render_template("home.html")
+
+
+
+if __name__ == '__main__':
+
+   
+    app.run(debug=True)
